caaa6f2fbb159539255076a2d43b44400fd0f795
Fady Essam <fadyesam1996@gmail.com>
2018-06-15 12:17:23 +0200

    reformatting the code and adding deoxygen comments

--- include/boost/numeric/ublas/opencl/opencl_core.hpp
+++ include/boost/numeric/ublas/opencl/opencl_core.hpp
@@ -6,213 +6,295 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/functional.hpp>
 #include <boost/compute/buffer.hpp>
-
-/// Include the clBLAS header. It includes the appropriate OpenCL headers
 #include <clBLAS.h>
 
 namespace boost {
-	namespace numeric {
-		namespace ublas {
+  namespace numeric {
+	namespace ublas {
 
 
-			namespace opencl
-			{
-				namespace compute = boost::compute;
-				namespace ublas = boost::numeric::ublas;
+	  namespace opencl
+	  {
+		namespace compute = boost::compute;
+		namespace ublas = boost::numeric::ublas;
+
+		/// a class that is used as a tag to tell that the matrix resides on a device
+		class storage;
 
 
-				class storage;
+		/** \breif this class is used to store information about a specific device
+		*
+		* the opencl library takes some overhead to get information about a devic and get it's context
+		* and command queue so we use this class to store these information for later accesing without consuming time.
+		*
+		*/
+		class opencl_device
+		{
 
-				class opencl_device
-				{
-				private:
-					compute::device device;
-					compute::context context;
-					compute::command_queue queue;
+		public:
+		  //Construction
 
-				public:
-					opencl_device() {}
-
-					opencl_device(unsigned int device_number)
-					{
-						this->device = compute::system::devices().at(device_number);
-						this->context = compute::context(device);
-						this->queue = compute::command_queue(context, device);
-					}
+		  /** initialize empty opencl_device
+		  */
+		  BOOST_UBLAS_INLINE
+			opencl_device() {}
 
 
-					void initialize(unsigned int device_number)
-					{
-						this->device = compute::system::devices().at(device_number);
-						this->context = compute::context(device);
-						this->queue = compute::command_queue(context, device);
-					}
+		  /** constructs opencl_device with information about device number device_number on the system
+		  * \param device_number the number of the device on the system
+		  */
+		  BOOST_UBLAS_INLINE
+			opencl_device(unsigned int device_number)
+		  {
+			this->device = compute::system::devices().at(device_number);
+			this->context = compute::context(device);
+			this->queue = compute::command_queue(context, device);
+		  }
 
-					compute::device& getDevice()
-					{
-						return device;
-					}
+		  /** initialize opencl_device to have information about device number device_number on the system
+		  * \param device_number the number of the device on the system
+		  */
+		  BOOST_UBLAS_INLINE
+			void initialize(unsigned int device_number)
+		  {
+			this->device = compute::system::devices().at(device_number);
+			this->context = compute::context(device);
+			this->queue = compute::command_queue(context, device);
+		  }
 
+		  ///returns boost::compute::device that has information about the device
+		  BOOST_UBLAS_INLINE
+			compute::device& getDevice()
+		  {
+			return device;
+		  }
 
-					const compute::device& getDevice() const
-					{
-						return device;
-					}
+		  ///returns boost::compute::device that has information about the device
+		  BOOST_UBLAS_INLINE
+			const compute::device& getDevice() const
+		  {
+			return device;
+		  }
 
-					compute::context& getContext()
-					{
-						return context;
-					}
+		  ///returns boost::compute::context that has information about the context of the device
+		  BOOST_UBLAS_INLINE
+			compute::context& getContext()
+		  {
+			return context;
+		  }
 
-					const compute::context& getContext() const
-					{
-						return context;
-					}
+		  ///returns boost::compute::context that has information about the context of the device
+		  BOOST_UBLAS_INLINE
+			const compute::context& getContext() const
+		  {
+			return context;
+		  }
 
-					const compute::command_queue& getQueue() const
-					{
-						return queue;
-					}
+		  ///returns boost::compute::command_queue that has information about the command queue of the device
+		  BOOST_UBLAS_INLINE
+			const compute::command_queue& getQueue() const
+		  {
+			return queue;
+		  }
 
-					compute::command_queue& getQueue()
-					{
-						return queue;
-					}
+		  ///returns boost::compute::command_queue that has information about the command queue of the device
+		  BOOST_UBLAS_INLINE
+			compute::command_queue& getQueue()
+		  {
+			return queue;
+		  }
 
+		private:
+		  compute::device device;
+		  compute::context context;
+		  compute::command_queue queue;
 
-
-				};
-
-				inline void startOpencl()
-				{
-					///start clBlas
-					clblasSetup();
-
-				}
-
-				inline void endOpencl()
-				{
-					/// Finalize clBLAS
-
-					clblasTeardown();
-
-				}
-
-		
-
-
-
-			}//opencl
-
-			 /**
-			 matrix class to keep data on gpu in case we need to do further computations on them
-			 */
-			template<class T, class L>
-			class matrix<T, L, opencl::storage> : public matrix_container<matrix<T, L, opencl::storage> >
-
-			{
-				typedef typename boost::compute::buffer_allocator<T>::size_type size_type;
-				typedef L layout_type;
-				typedef matrix<T, L, opencl::storage> self_type;
-
-			public:
-				matrix(size_type size1, size_type size2, opencl::opencl_device const &d)
-					: matrix_container<self_type>(),
-					size1_(size1), size2_(size2), data_(compute::vector<T>(layout_type::storage_size(size1, size2), d.getContext())), device_(d)
-				{}
-
-				matrix(size_type size1, size_type size2, opencl::opencl_device &d, const T& value)
-					: matrix_container<self_type>(),
-					size1_(size1), size2_(size2), data_(compute::vector<T>(layout_type::storage_size(size1, size2), d.getContext())), device_(d)
-				{
-					compute::fill(data_.begin(), data_.end(), value, d.getQueue());
-				}
+		};
 
 
-				matrix(opencl::opencl_device& d)
-					: matrix_container<self_type>(),
-					size1_(0), size2_(0), data_(compute::vector<T>(d.getContext())), device_(d)
-				{}
+		///start clBlas (used for the matrix product kernel
+		BOOST_UBLAS_INLINE
+		  inline void startOpencl()
+		{
+		  clblasSetup();
+		}
 
-				// Accessors
-				/** Return the number of rows of the matrix
-				*/
-				BOOST_UBLAS_INLINE
-					size_type size1() const {
-					return size1_;
-				}
-
-				/** Return the number of colums of the matrix
-				*/
-				BOOST_UBLAS_INLINE
-					size_type size2() const {
-					return size2_;
-				}
-
-				/** Return the compute::vector that has the data on gpu
-				*/
-				const compute::vector<T> &data() const { return data_; }
-				compute::vector<T> &data() { return data_; }
-
-				/** Return the device that the matrix resides on
-				*/
-				const opencl::opencl_device &device() const { return device_; }
-				opencl::opencl_device &device() { return device_; }
-
-
-				/** Resize the matrix
-				*/
-				void resize(size_type size1, size_type size2)
-				{
-					size1_ = size1;
-					size2_ = size2;
-					data_.resize(layout_type::storage_size(size1, size2));
-				}
-
-				/**
-				Fill all elements of the matrix with the value
-				*/
-				void fill(T value)
-				{
-					compute::fill(data_.begin(), data_.end(), value, device_.getQueue());
-				}
-
-
-				template<class A>
-				void to_host(ublas::matrix<T, L, A>& m)
-				{
-					this->resize(m.size1(), m.size2());
-					compute::copy(
-						m.data().begin(),
-						m.data().end(),
-						this->data().begin(),
-						device_.getQueue()
-					);
-				}
+		/// Finalize clBLAS (used for the matrix product kernel)
+		BOOST_UBLAS_INLINE
+		  inline void endOpencl()
+		{
+		  clblasTeardown();
+		}
 
 
 
-				template<class A>
-				 void from_host(ublas::matrix<T, L, A>& m)
-				{
-					 m.resize(size1_, size2_);
-					compute::copy(
-						this->data().begin(),
-						this->data().end(),
-						m.data().begin(),
-						device_.getQueue()
-					);
-				}
 
 
-			private:
-				size_type size1_;
-				size_type size2_;
-				compute::vector<T> data_;
-				opencl::opencl_device device_;
-			};
-		}//ublas
-	}//numeric
+
+	  }//opencl
+
+	   /** \brief A dense matrix of values of type \c T that resides on a specific device using opencl.
+	   *
+	   * this is a special case of class boost::numeric::ublas::matrix<T, L, A> that has A as opencl::storage
+	   * so it is stored on a specific device and stores some information about this device.
+	   *
+	   *
+	   * For a \f$(m \times n)\f$-dimensional matrix and \f$ 0 \leq i < m, 0 \leq j < n\f$, every element \f$ m_{i,j} \f$ is mapped to
+	   * the \f$(i.n + j)\f$-th element of the container for row major orientation or the \f$ (i + j.m) \f$-th element of
+	   * the container for column major orientation.
+	   *
+	   *
+	   * Orientation and storage can also be specified, otherwise a \c row_major and \c unbounded_array are used. It is \b not
+	   * required by the storage to initialize elements of the matrix.
+	   *
+	   * \tparam T the type of object stored in the matrix (like double, float, complex, etc...)
+	   * \tparam L the storage organization. It can be either \c row_major or \c column_major. Default is \c row_major
+	   */
+	  template<class T, class L>
+	  class matrix<T, L, opencl::storage> : public matrix_container<matrix<T, L, opencl::storage> >
+
+	  {
+		typedef typename boost::compute::buffer_allocator<T>::size_type size_type;
+		typedef L layout_type;
+		typedef matrix<T, L, opencl::storage> self_type;
+
+	  public:
+
+		// Construction
+
+
+		/** Dense matrix constructor with size (0,0)
+		* \param d is the boost::nemeric::ublas::opencl::opencl_device that contains the matrix on its memory
+		*/
+		BOOST_UBLAS_INLINE
+		  matrix(opencl::opencl_device& d)
+		  : matrix_container<self_type>(),
+		  size1_(0), size2_(0), data_(compute::vector<T>(d.getContext())), device_(d)
+		{}
+
+		/** Dense matrix constructor with size (size1,size2) and resides on memory of device d
+		* \param d is the boost::nemeric::ublas::opencl::opencl_device that contains the matrix on its memory
+		 * \param size1 number of rows
+		 * \param size2 number of columns
+		*/
+		BOOST_UBLAS_INLINE
+		  matrix(size_type size1, size_type size2, opencl::opencl_device &d)
+		  : matrix_container<self_type>(),
+		  size1_(size1), size2_(size2), data_(compute::vector<T>(layout_type::storage_size(size1, size2), d.getContext())), device_(d)
+		{}
+
+
+		/** Dense matrix constructor with size (size1,size2) and resides on memory of device d and initialize all elements to value
+		* \param d is the boost::nemeric::ublas::opencl::opencl_device that contains the matrix on its memory
+		* \param size1 number of rows
+		* \param size2 number of columns
+		* \param value is the value that all elements of the matrix are set to
+		*/
+		BOOST_UBLAS_INLINE
+		  matrix(size_type size1, size_type size2, opencl::opencl_device &d, const T& value)
+		  : matrix_container<self_type>(),
+		  size1_(size1), size2_(size2), data_(compute::vector<T>(layout_type::storage_size(size1, size2), d.getContext())), device_(d)
+		{
+		  compute::fill(data_.begin(), data_.end(), value, d.getQueue());
+		}
+
+
+		// Accessors
+		/** Return the number of rows of the matrix
+		*/
+		BOOST_UBLAS_INLINE
+		  size_type size1() const {
+		  return size1_;
+		}
+
+		/** Return the number of colums of the matrix
+		*/
+		BOOST_UBLAS_INLINE
+		  size_type size2() const {
+		  return size2_;
+		}
+
+		/** Return the compute::vector that has the data on the device
+		*/
+		BOOST_UBLAS_INLINE
+		  const compute::vector<T> &data() const { return data_; }
+
+		BOOST_UBLAS_INLINE
+		  compute::vector<T> &data() { return data_; }
+
+		/** Return boost::numeri::ublas::opencl::opencl_device that has informaton
+		* about the device that the matrix resides on.
+		*/
+		BOOST_UBLAS_INLINE
+		  const opencl::opencl_device &device() const { return device_; }
+
+		BOOST_UBLAS_INLINE
+		  opencl::opencl_device &device() { return device_; }
+
+
+		/** Resize the matrix to specific size
+		* \param size1 new number of rows
+		* \param size2 new number of columns
+		*/
+		BOOST_UBLAS_INLINE
+		  void resize(size_type size1, size_type size2)
+		{
+		  size1_ = size1;
+		  size2_ = size2;
+		  data_.resize(layout_type::storage_size(size1, size2));
+		}
+
+		/**Fill all elements of the matrix with the value
+		* \param value value to set all elements of the matrix to
+		*/
+		BOOST_UBLAS_INLINE
+		  void fill(T value)
+		{
+		  compute::fill(data_.begin(), data_.end(), value, device_.getQueue());
+		}
+
+		/** Copies a matrix to a device
+		* \param m is a matrix that is not on the device _device and it is copied to it
+		*/
+		BOOST_UBLAS_INLINE
+		  template<class A>
+		void to_host(ublas::matrix<T, L, A>& m)
+		{
+		  this->resize(m.size1(), m.size2());
+		  compute::copy(
+			m.data().begin(),
+			m.data().end(),
+			this->data().begin(),
+			device_.getQueue()
+		  );
+		}
+
+
+		/** Copies a matrix from a device
+		* \param m is a matrix that will be reized to (size1_,size2) and the values of (*this) will be copied in it
+		*/
+		BOOST_UBLAS_INLINE
+		  template<class A>
+		void from_host(ublas::matrix<T, L, A>& m)
+		{
+		  m.resize(size1_, size2_);
+		  compute::copy(
+			this->data().begin(),
+			this->data().end(),
+			m.data().begin(),
+			device_.getQueue()
+		  );
+		}
+
+
+	  private:
+		size_type size1_;
+		size_type size2_;
+		compute::vector<T> data_;
+		opencl::opencl_device device_;
+	  };
+	}//ublas
+  }//numeric
 }//boost
 
 
--- include/boost/numeric/ublas/opencl/operations.hpp
+++ include/boost/numeric/ublas/opencl/operations.hpp
@@ -11,165 +11,167 @@
 #include <clBLAS.h>
 
 namespace boost {
-	namespace numeric {
-		namespace ublas {
+  namespace numeric {
+	namespace ublas {
 
 
-			namespace opencl
-			{
-				namespace compute = boost::compute;
-				namespace ublas = boost::numeric::ublas;
+	  namespace opencl
+	  {
+		namespace compute = boost::compute;
+		namespace ublas = boost::numeric::ublas;
+
+	    #define ONE_DOUBLE_COMPLEX  { { 1.0, 00.0 } }
+	    #define ONE_FLOAT_COMPLEX  { { 1.0f, 00.0f } }
+
+		/**This function computes the product of 2 matrices (a*b) and stores it at matrix result all 3 matrices are on device
+		*
+		* a and b are originally on device (on the same device) and the result is left on the same device.
+		*
+		* \param a matrix A of the product (A*B) that is on opencl_device device
+		* \param b matrix B of the product (A*B) that is on the opencl_device device
+		* \param result matrix on device to store the product of the result of (A*B)
+		*
+		* \tparam T datatype of the matrices
+		* \tparam L layout of the matrices (row_majot or column_major)
+		*/
+		template <class T, class F>
+		BOOST_UBLAS_INLINE
+		  void prod(ublas::matrix<T, F, opencl::storage>& a, ublas::matrix<T, F, opencl::storage>& b, ublas::matrix<T, F, opencl::storage>& result)
+		{
 
 
+		  //get data from device
+		  compute::device device = a.device().getDevice();
+		  compute::context context = a.device().getContext();
+		  compute::command_queue queue = a.device().getQueue();
 
 
-#define ONE_DOUBLE_COMPLEX  { { 1.0, 00.0 } }
-#define ONE_FLOAT_COMPLEX  { { 1.0f, 00.0f } }
+		  result.resize(a.size1(), b.size2());
+		  result.fill(0);
 
+		  cl_event event = NULL;
 
-
-
-				/**
-				This function computes the prodect of 2 matrices (A*B) and stores it at matrix result all 3 matrices are on device
-				a and b are originally on gpu (on the same device) and the result is left on the same device
-				*/
-				template <class T, class F>
-				BOOST_UBLAS_INLINE
-					void prod(ublas::matrix<T, F, opencl::storage>& a, ublas::matrix<T, F, opencl::storage>& b, ublas::matrix<T, F, opencl::storage>& result)
-				{
-
-
-					///get data from device
-					compute::device device = a.device().getDevice();
-					compute::context context = a.device().getContext();
-					compute::command_queue queue = a.device().getQueue();
-
-
-					result.resize(a.size1(), b.size2());
-					result.fill(0);
-
-					cl_int err;
-					cl_event event = NULL;
-
-					clblasOrder Order = std::is_same<F, ublas::basic_row_major<> >::value ? clblasRowMajor : clblasColumnMajor;
-					int lda = Order == clblasRowMajor ? a.size2() : a.size1();
-					int ldb = Order == clblasRowMajor ? b.size2() : a.size2();
-					int ldc = Order == clblasRowMajor ? b.size2() : a.size1();
-
-
-
-					if (std::is_same<T, float>::value)
-						///Call clBLAS extended function. Perform gemm for float
-						err = clblasSgemm(Order, clblasNoTrans, clblasNoTrans,
-							a.size1(), b.size2(), a.size2(),
-							1, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
-							(cl_mem)b.data().begin().get_buffer().get(), 0, ldb, 1,
-							(cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
-							1, &(queue.get()), 0, NULL, &event);
-
-
-					else if (std::is_same<T, double>::value)
-						///Call clBLAS extended function. Perform gemm for double
-						err = clblasDgemm(Order, clblasNoTrans, clblasNoTrans,
-							a.size1(), b.size2(), a.size2(),
-							1, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
-							(cl_mem)b.data().begin().get_buffer().get(), 0, ldb, 1,
-							(cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
-							1, &(queue.get()), 0, NULL, &event);
-
-					else if (std::is_same<T, std::complex<float>>::value)
-						///Call clBLAS extended function. Perform gemm for double
-						err = clblasCgemm(Order, clblasNoTrans, clblasNoTrans,
-							a.size1(), b.size2(), a.size2(),
-							ONE_FLOAT_COMPLEX, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
-							(cl_mem)b.data().begin().get_buffer().get(), 0, ldb, ONE_FLOAT_COMPLEX,
-							(cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
-							1, &(queue.get()), 0, NULL, &event);
-
-					else if (std::is_same<T, std::complex<double>>::value)
-						///Call clBLAS extended function. Perform gemm for double
-						err = clblasZgemm(Order, clblasNoTrans, clblasNoTrans,
-							a.size1(), b.size2(), a.size2(),
-							ONE_DOUBLE_COMPLEX, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
-							(cl_mem)b.data().begin().get_buffer().get(), 0, ldb, ONE_DOUBLE_COMPLEX,
-							(cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
-							1, &(queue.get()), 0, NULL, &event);
+		  clblasOrder Order = std::is_same<F, ublas::basic_row_major<> >::value ? clblasRowMajor : clblasColumnMajor;
+		  int lda = Order == clblasRowMajor ? a.size2() : a.size1();
+		  int ldb = Order == clblasRowMajor ? b.size2() : a.size2();
+		  int ldc = Order == clblasRowMajor ? b.size2() : a.size1();
 
 
 
-					///Wait for calculations to be finished.
-					clWaitForEvents(1, &event);
+		  if (std::is_same<T, float>::value)
+			//Call clBLAS extended function. Perform gemm for float
+			clblasSgemm(Order, clblasNoTrans, clblasNoTrans,
+			  a.size1(), b.size2(), a.size2(),
+			  1, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
+			  (cl_mem)b.data().begin().get_buffer().get(), 0, ldb, 1,
+			  (cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
+			  1, &(queue.get()), 0, NULL, &event);
+
+
+		  else if (std::is_same<T, double>::value)
+			//Call clBLAS extended function. Perform gemm for double
+			clblasDgemm(Order, clblasNoTrans, clblasNoTrans,
+			  a.size1(), b.size2(), a.size2(),
+			  1, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
+			  (cl_mem)b.data().begin().get_buffer().get(), 0, ldb, 1,
+			  (cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
+			  1, &(queue.get()), 0, NULL, &event);
+
+		  else if (std::is_same<T, std::complex<float>>::value)
+			//Call clBLAS extended function. Perform gemm for double
+			clblasCgemm(Order, clblasNoTrans, clblasNoTrans,
+			  a.size1(), b.size2(), a.size2(),
+			  ONE_FLOAT_COMPLEX, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
+			  (cl_mem)b.data().begin().get_buffer().get(), 0, ldb, ONE_FLOAT_COMPLEX,
+			  (cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
+			  1, &(queue.get()), 0, NULL, &event);
+
+		  else if (std::is_same<T, std::complex<double>>::value)
+			//Call clBLAS extended function. Perform gemm for double
+			clblasZgemm(Order, clblasNoTrans, clblasNoTrans,
+			  a.size1(), b.size2(), a.size2(),
+			  ONE_DOUBLE_COMPLEX, (cl_mem)a.data().begin().get_buffer().get(), 0, lda,
+			  (cl_mem)b.data().begin().get_buffer().get(), 0, ldb, ONE_DOUBLE_COMPLEX,
+			  (cl_mem)result.data().begin().get_buffer().get(), 0, ldc,
+			  1, &(queue.get()), 0, NULL, &event);
 
 
 
-				}
+		  //Wait for calculations to be finished.
+		  clWaitForEvents(1, &event);
+
+
+
+		}
 
 
 
 
-				/**
-				This function computes the prodect of 2 matrices (A*B) and stores it at matrix result and keeps result on device
-				it first transfers the data of matrix a,b to the device and execute a clBlas kernel according to
-				the data type of the matrices to multiply them
-				*/
-				template <class T, class F, class A>
-				BOOST_UBLAS_INLINE
-					void prod(ublas::matrix<T, F, A>& a, ublas::matrix<T, F, A>& b, ublas::matrix<T, F, opencl::storage>& result, opencl_device& device)
-				{
+		
+		/**This function computes the product of 2 matrices not on device (a*b) and stores it at matrix result which is also not on device
+		*
+		* a and b are originally not on device so they are copied to device and the evice does computatons on them and the result is copied to matrix result
+		*
+		* \param a matrix A of the product (A*B) that is not on opencl_device device
+		* \param b matrix B of the product (A*B) that is not on the opencl_device device
+		* \param result matrix on device to store the product of the result of (A*B)
+		* \param device has the information about the device which as the two matrices and which will do the computation
+		*
+		* \tparam T datatype of the matrices
+		* \tparam L layout of the matrices (row_majot or column_major)
+		* \tparam A storage type that has the data of the matrices
+		*/
+		template <class T, class F, class A>
+		BOOST_UBLAS_INLINE
+		  void prod(ublas::matrix<T, F, A>& a, ublas::matrix<T, F, A>& b, ublas::matrix<T, F, A>& result, opencl_device& device)
+		{
 
-					
+		  ///copy the data from a to aHolder
+		  ublas::matrix<T, F, opencl::storage> aHolder(device);
+		  aHolder.to_host(a);
 
-				}
+		  ///copy the data from b to bHolder
+		  ublas::matrix<T, F, opencl::storage> bHolder(device);
+		  bHolder.to_host(b);
 
+		  ublas::matrix<T, F, opencl::storage> resultHolder(device);
 
-				/**
-				This function computes the prodect of 2 matrices (A*B) and stores it at matrix result (all 3 matrices are not on device
-				it first transfers the data of matrix a,b to the device and execute a clBlas kernel according to
-				the data type of the matrices to multiply them
-				*/
-				template <class T, class F, class A>
-				BOOST_UBLAS_INLINE
-					void prod(ublas::matrix<T, F, A>& a, ublas::matrix<T, F, A>& b, ublas::matrix<T, F, A>& result, opencl_device& device)
-				{
+		  prod(aHolder, bHolder, resultHolder); //call the prod function that multiplies a function already on gpu
 
-					///copy the data from a to aHolder
-					ublas::matrix<T, F, opencl::storage> aHolder(device);
-					aHolder.to_host(a);
-
-					///copy the data from b to bHolder
-					ublas::matrix<T, F, opencl::storage> bHolder(device);
-					bHolder.to_host(b);
-
-					ublas::matrix<T, F, opencl::storage> resultHolder(device);
-
-					prod(aHolder, bHolder, resultHolder); //call the prod function that multiplies a function already on gpu
-
-					resultHolder.from_host(result);
+		  resultHolder.from_host(result);
 
 
-				}
+		}
 
 
-				/**
-				This function computes the prodect of 2 matrices (A*B) that are not on gpu and returns the result
-				it uses the function prod(A,B,result) and returns result
-				*/
+		/**This function computes the product of 2 matrices not on device (a*b) and stores it at matrix result which is also not on device
+		*
+		* a and b are originally not on device so they are copied to device and the evice does computatons on them and the result is copied from device returned
+		*
+		* \param a matrix A of the product (A*B) that is not on opencl_device device
+		* \param b matrix B of the product (A*B) that is not on the opencl_device device
+		* \param device has the information about the device which as the two matrices and which will do the computation
+		*
+		* \tparam T datatype of the matrices
+		* \tparam L layout of the matrices (row_majot or column_major)
+		* \tparam A storage type that has the data of the matrices
+		*/
 
-				template <class T, class F, class A>
-				BOOST_UBLAS_INLINE
-					ublas::matrix<T, F, A> prod(ublas::matrix<T, F, A>& a, ublas::matrix<T, F, A>& b, opencl_device device)
-				{
-					ublas::matrix<T, F, A> result;
-					prod(a, b, result, device);
-					return result;
-				}
+		template <class T, class F, class A>
+		BOOST_UBLAS_INLINE
+		  ublas::matrix<T, F, A> prod(ublas::matrix<T, F, A>& a, ublas::matrix<T, F, A>& b, opencl_device device)
+		{
+		  ublas::matrix<T, F, A> result;
+		  prod(a, b, result, device);
+		  return result;
+		}
 
 
 
-			}//opencl
+	  }//opencl
 
-		}//ublas
-	}//numeric
+	}//ublas
+  }//numeric
 }//boost
 
 
