ad052ecd662cffd0d4f9d8c1c247b2b2720e8887
Fady Essam <fadyesam1996@gmail.com>
2018-06-20 23:14:17 +0200

    trying testing doubles on ci servers

--- test/opencl/prod_test.cpp
+++ test/opencl/prod_test.cpp
@@ -4,41 +4,45 @@
 int main()
 {
 
-	///testing row major flaot prod
-	bench_prod<float, ublas::basic_row_major<>, 10, 10> b1;
+  ///testing row major flaot prod
+  bench_prod<float, ublas::basic_row_major<>, 10, 10> b1;
 
-	///testing row major complex float prod
-	bench_prod<std::complex<float>, ublas::basic_row_major<>, 10, 10> b2;
-
-
-	///testing row major double prod
-	bench_prod<double, ublas::basic_row_major<>, 10, 10> b5;
-
-	///testing row major complex float prod
-	bench_prod<std::complex<double>, ublas::basic_row_major<>, 10, 10> b6;
+  ///testing row major complex float prod
+  bench_prod<std::complex<float>, ublas::basic_row_major<>, 10, 10> b2;
 
 
-	///testing column major flaot prod
-	bench_prod<float, ublas::basic_column_major<>, 10, 10> b3;
+  ///testing row major double prod
+  bench_prod<double, ublas::basic_row_major<>, 10, 10> b5;
 
-	///testing column major complex float prod
-	bench_prod<std::complex<float>, ublas::basic_column_major<>, 10, 10> b4;
-
-	///testing column major double prod
-	bench_prod<double, ublas::basic_column_major<>, 10, 10> b3;
-
-	///testing column major complex double prod
-	bench_prod<std::complex<double>, ublas::basic_column_major<>, 10, 10> b4;
+  ///testing row major complex float prod
+  bench_prod<std::complex<double>, ublas::basic_row_major<>, 10, 10> b6;
 
 
-	std::cout << "Row major:" << std::endl;
-	b1.run();
-	b2.run();
+  ///testing column major flaot prod
+  bench_prod<float, ublas::basic_column_major<>, 10, 10> b3;
 
-	std::cout << "Column major:" << std::endl;
-	b3.run();
-	b4.run();
+  ///testing column major complex float prod
+  bench_prod<std::complex<float>, ublas::basic_column_major<>, 10, 10> b4;
 
-		return 0;
+  ///testing column major double prod
+  bench_prod<double, ublas::basic_column_major<>, 10, 10> b7;
+
+  ///testing column major complex double prod
+  bench_prod<std::complex<double>, ublas::basic_column_major<>, 10, 10> b8;
+
+
+  std::cout << "Row major:" << std::endl;
+  b1.run();
+  b2.run();
+  b5.run();
+  b6.run();
+
+  std::cout << "Column major:" << std::endl;
+  b3.run();
+  b4.run();
+  b7.run();
+  b8.run();
+
+  return 0;
 
 }
